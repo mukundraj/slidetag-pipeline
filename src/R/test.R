06aa972da083a9e858477a020ab6249d8d606a8d
Mukund Raj <mraj@broadinstitute.org>
2023-05-24 18:56:46 +0000

    Create an r_env_ubun specifically for ubuntu instance
    Also add command to disable an unsolicited pdf Rplots.pdf being generated in work dir

--- src/R/test.R
+++ src/R/test.R
@@ -1,6 +1,8 @@
-
 impath <- snakemake@output[['opimg']]
 library(ggplot2)
+
+pdf(NULL) # to stop the unsolicited Rplots.pdf being generated in working dir
+
 ggplot(diamonds)  # if only the dataset is known.
 ggplot(diamonds, aes(x=carat))  # if only X-axis is known. The Y-axis can be specified in respective geoms.
 ggplot(diamonds, aes(x=carat, y=price))  # if both X and Y axes are fixed for all layers.
