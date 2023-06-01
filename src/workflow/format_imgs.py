559573855509510dbd5a4d406938bdcf644e4dd3
Mukund Raj <mraj@broadinstitute.org>
2023-06-01 11:55:15 -0400

    Remove alpha channel and make stag imgs white

--- src/workflow/format_imgs.py
+++ src/workflow/format_imgs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # print('python', snakemake.input[0])
-
 import os
-import glob,os
+import glob
+
 
 print('formatting imgs, ip:', snakemake.input[0], 'op:', snakemake.output[0])
 print('formatting imgs, ip:', snakemake.input[1], 'op:', snakemake.output[0])
@@ -22,7 +22,7 @@ for file in glob.glob("*.png"):
     ip_file_path = os.path.join(inp_stags_dir, file)
     op_file_name = file.split('.')[0] + '.tif'
     op_file_path = os.path.join(fmted_stags_dir, op_file_name)
-    os.system(f'convert {ip_file_path} {op_file_path}')
+    os.system(f'convert -background white -alpha remove -alpha off {ip_file_path} {op_file_path}')
 
 inp_nis_dir = snakemake.input[1]
 fmted_nis_dir = f'{snakemake.output[0]}/nissls'
@@ -37,7 +37,7 @@ for file in nis_files:
     ip_file_path = os.path.join(inp_nis_dir, file)
     op_file_name =  f'{snakemake.config["dataname"]}_nissl.tif'
     op_file_path = os.path.join(fmted_nis_dir, op_file_name)
-    os.system(f'convert {ip_file_path} {op_file_path}')
+    os.system(f'convert -alpha off {ip_file_path} {op_file_path}')
 
 
 
